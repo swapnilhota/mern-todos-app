4d314a4ec297f2fe3a3251eaee8dbff77e0e1ba0
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-20 09:50:18 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
 import cors from 'cors';
 
 const app = express();
@@ -11,3 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 const mongodb = "mongodb+srv://first-user:swapnil123@cluster0.t01a9.mongodb.net/mern-todo-app?retryWrites=true&w=majority";
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the Server");
+})
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(mongodb)
+    .then(() => {
+        console.log(`server is running on port ${PORT}`);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
