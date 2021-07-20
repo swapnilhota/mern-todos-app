9a1db40a0749872c34e16f786923687078d64077
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-20 10:06:51 +0530

    running the server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
@@ -10,7 +10,7 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const mongodb = "mongodb+srv://first-user:swapnil123@cluster0.t01a9.mongodb.net/mern-todo-app?retryWrites=true&w=majority";
+const mongodb = "mongodb+srv://first-user:mongodb@cluster0.t01a9.mongodb.net/mern-todo-app?retryWrites=true&w=majority";
 
 app.get('/', (req, res) => {
     res.send("Welcome to the Server");
@@ -18,10 +18,12 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(mongodb)
+mongoose.connect(mongodb, { useUnifiedTopology: true }, { useNewUrlParser: true })
     .then(() => {
         console.log(`server is running on port ${PORT}`);
     })
     .catch((err) => {
         console.log(err);
     })
+
+app.listen(PORT);
