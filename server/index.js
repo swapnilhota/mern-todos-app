48c447c28121e8af4fb7fb4dbfe392a83d2c25b5
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-20 09:46:44 +0530

    wired mongodb

--- server/index.js
+++ server/index.js
@@ -8,3 +8,6 @@ dotenv.config();
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const mongodb = "mongodb+srv://first-user:swapnil123@cluster0.t01a9.mongodb.net/mern-todo-app?retryWrites=true&w=majority";
