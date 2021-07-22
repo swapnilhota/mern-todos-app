5327e8c4962edbc6bb80027f7926b1f685be69fb
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-22 10:27:46 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,13 @@ function App() {
           </div>
         </form>
         <Preloader />
+        <div className="collection">
+          <a href="#!" className="collection-item">Alvin</a>
+          <a href="#!" className="collection-item active">Alvin</a>
+          <a href="#!" className="collection-item">Alvin</a>
+          <a href="#!" className="collection-item">Alvin</a>
+        </div>
+
       </div>
     </div>
   );
