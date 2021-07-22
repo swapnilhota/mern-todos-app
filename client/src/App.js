950f95f3b20ca2468bd1a0dedb0177f91148cbdb
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-22 11:02:09 +0530

    upate

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import { useEffect } from 'react';
-import { readTodos } from './functions';
+import { useEffect, useState } from 'react';
+import { createTodo, readTodos } from './functions';
 import Preloader from './components/Preloader.js';
 
 function App() {
+
+  const [todo, setTodo] = useState({ title: '', content: '' });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,10 +14,17 @@ function App() {
     fetchData();
   }, [])
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    const result = await createTodo(todo);
+    console.log(result);
+  }
+
   return (
     <div className="container">
       <div className="row">
-        <form className="col s12">
+        <form className="col s12" onSubmit={onSubmitHandler}>
           <div className="row">
             <div className="input-field col s6">
               <i className="material-icons prefix">title</i>
@@ -27,6 +36,9 @@ function App() {
               <input id="description" type="tel" className="validate" />
               <label htmlFor="description">Content</label>
             </div>
+          </div>
+          <div className="row right-align">
+            <button className="waves-effect waves-light">Submit</button>
           </div>
         </form>
         <Preloader />
