8d48c242cdf9e1d78ff672d6e09839ba6f137696
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-24 10:36:33 +0530

    update

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,4 +2,7 @@ import axios from 'axios';
 const url = 'http://localhost:3000/todos';
 
 export const readTodos = () => axios.get(url);
+
 export const createTodo = (newTodo) => axios.post(url, newTodo);
+
+export const deleteTodo = (id) => axios.delete(`${url}/${id}`);
